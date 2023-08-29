7bb09b5e3f3d4f79648d225bb2d8f6b047115f33
Ali_AlienWare <aliasgermhow@gmail.com>
2023-08-28 21:08:47 -0700

    NumPy Tutorial, save for 8/28/23

--- NumPy_Practice.py
+++ NumPy_Practice.py
@@ -1,3 +1,19 @@
 # File created to practice using Python's Numpy Library
 import numpy as np
 import sys
+
+#The Basics
+
+a= np.array([1,2,3],dtype='int16')
+b = np.array([[9.0,8.0,7.0],[6.0,5.0,4.0]])
+
+#Get Dimensions
+print(a.ndim)
+print(b.ndim)
+
+#Get Shape
+print(a.shape)
+print(b.shape)
+
+#Get Type
+print(a.dtype)
